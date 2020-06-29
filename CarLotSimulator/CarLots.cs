1aad72ee7244d96a073421c03bb13b59ce6e89f3
Brenton Phillips <phillips.brenton@gmail.com>
2020-06-29 15:05:58 -0500

    added classes Car and CarLot

---
+++ CarLotSimulator/CarLots.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarLotSimulator
+{
+    public class CarLots
+    {
+        public CarLots()
+        {
+        }
+
+        public List<Car> Cars = new List<Car>();
+    }
+}
